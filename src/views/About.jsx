9b7f7381dd5325d40c81bbe8b2f18f5a7d70fbb0
nvonbuttlar <nick.von.buttlar@gmail.com>
2025-02-09 11:27:56 -0800

    design

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,6 +2,12 @@ export default function About() {
   return (
     <div className="view">
       <h1>About</h1>
+      <div>
+        <h6>Hi there, welcome to my portfolio.</h6>
+        <p>Here you will find a variety of information about me so you can better understand me as a developer, a creator, and most importantly - as a human being! The primary focus of this portfolio is to showcase my experience and expertise as a developer. But why stop there? This portfolio is also going to serve as a sort of 'digital scrapbook' to host creative projects of all varieties, wheather it be music, a game, or a new useless widget.</p>
+        <p>Click around and enjoy!</p>
+        <p>- Nick</p>
+      </div>
     </div>
   )
 }
