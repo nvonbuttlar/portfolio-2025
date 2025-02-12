293451bfab9afe1ed5271f69f978b84b7a5b6dc4
nvonbuttlar <nick.von.buttlar@gmail.com>
2025-02-11 20:58:44 -0800

    build mobile nav

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -2,6 +2,11 @@ export default function NavList({showMobileMenu}) {
 
   return (
     <ul className={`nav__list ${showMobileMenu ? "nav__list--active" : ""}`}>
+      {showMobileMenu && (
+        <li className="nav__link">
+          <a href="/" className="logo">Nickvb.xyz</a>
+        </li>
+      )}
       <li className="nav__link">
         <a href="/about">About</a>
       </li>
