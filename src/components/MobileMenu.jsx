c34364a81d2a78ee76a6eb2441de2d822bf75469
nvonbuttlar <nick.von.buttlar@gmail.com>
2025-02-11 17:32:09 -0800

    remove whitespace from bottom of page

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -3,7 +3,7 @@ import NavList from "./NavList";
 export default function MobileMenu({showMobileMenu}) {
   return (
     <div className={`mobile-menu ${showMobileMenu ? "mobile-menu--active" : ""}`}>
-      <div>Nickvb.xyz</div>
+      <p>Nickvb.xyz</p>
       <NavList showMobileMenu={showMobileMenu} />
     </div>
   );
