c6746e5529d3cf3ee55cdc8a4e150818d51be90c
nvonbuttlar <nick.von.buttlar@gmail.com>
2025-02-25 14:03:38 -0800

    build UI for radio feature

--- src/components/Radio.jsx
+++ src/components/Radio.jsx
@@ -1,13 +1,33 @@
-import RadioImage from "../assets/retro-radio.webp";
+import RadioImage from "../assets/gold-radio.png";
 import { MusicalNoteIcon } from '@heroicons/react/16/solid';
+import { XMarkIcon } from '@heroicons/react/16/solid';
+import { useState } from "react";
 
 export default function Radio() {
+  const [showRadio, setShowRadio] = useState(false);
+
+  const toggleRadio = () => {
+    setShowRadio(!showRadio);
+  }
+
+  const closeRadio = () => {
+    setShowRadio(false);
+  }
+
   return (
     <>
-      <div className="radio-tab">
+      {showRadio && (
+        <div className="overlay overlay__radio"></div>
+      )}
+      <div className="radio-tab" onClick={() => toggleRadio()}>
         <MusicalNoteIcon className='icon' />
       </div>
-      {/* <img className="radio" src={RadioImage} alt={"Retro Radio"} /> */}
+      <div className={`radio ${showRadio ? 'radio--active': ''}`}>
+        {showRadio && (
+          <XMarkIcon className='icon' onClick={() => closeRadio() } />
+        )}
+        <img className="radio__img" src={RadioImage} alt={"Retro Radio"} />
+      </div>
     </>
   )
 }
