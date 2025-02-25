e9a90977732e2b9bfb9200a1a2ba75e097b7c8e4
nvonbuttlar <nick.von.buttlar@gmail.com>
2025-02-24 19:10:30 -0800

    make skill grids responsive

---
+++ src/components/SkillBar.jsx
@@ -0,0 +1,11 @@
+export default function SkillBar({icon, title, progress}) {
+  return (
+    <li className="skill">
+      <img className="skill__icon" src={icon} alt={title} />
+      <div className="skill__stats">
+        <h6>{title}</h6>
+        <progress className="skill__progress" value={progress} max={100} />
+      </div>
+    </li>
+  );
+}
--- src/views/Skills.jsx
+++ src/views/Skills.jsx
@@ -1,14 +1,33 @@
 import ResumeButtons from "../components/ResumeButtons";
+import SkillBar from "../components/SkillBar";
+import Html5 from "../assets/html5-48.png";
 
 export default function Skills() {
   return (
-    <div className="view">
+    <div className="view skills">
       <div className='view__header'>
         <h1>Skills</h1>
       </div>
       <div className='view__content'>
-        <ResumeButtons />
-        <progress value={75} max={100} />
+        <ResumeButtons />        
+        <h3 className="underline">Frontend</h3>
+        <ul className="skills__group">
+          <SkillBar 
+            icon={Html5}
+            title={"HTML5"}
+            progress={95}
+          />
+          <SkillBar 
+            icon={Html5}
+            title={"HTML5"}
+            progress={95}
+          />
+          <SkillBar 
+            icon={Html5}
+            title={"HTML5"}
+            progress={95}
+          />
+        </ul>
       </div>
     </div>
   )
